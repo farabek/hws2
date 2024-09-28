def05989f7a6765881d58a65ccea8ad7653a41f8
farabek <farhodmuhamadiev4@gmail.com>
2024-09-28 12:15:21 +0500

    change all files

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -111,7 +111,7 @@ export const friendMessage0: MessageType = {
   },
   message: {
     text: ' Hello, how are you, what did you do yesterday?', // можно менять
-    time: '22:05', // можно менять
+    time: '22:10', // можно менять
   },
 };
 
