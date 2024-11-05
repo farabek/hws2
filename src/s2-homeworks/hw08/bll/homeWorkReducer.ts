177a583f262685cc8e3c37664f2705ac67d6c399
farabek <farhodmuhamadiev4@gmail.com>
2024-11-05 20:13:50 +0500

    made HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,26 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8';
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+// export const homeWorkReducer = (state: any, action: any): any => {
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case 'sort': {
+      // by name
+      const sortedState = [...state].sort((a, b) =>
+        action.payload === 'up' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name),
+      );
+      //   return state; // need to fix
+      return sortedState;
     }
-}
+    case 'check': {
+      //   return state; // need to fix
+      return state.filter((user) => user.age >= action.payload);
+    }
+    default:
+      return state;
+  }
+};
+
+//
