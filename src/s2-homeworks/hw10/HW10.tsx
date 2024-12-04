0f35a0bd7a9cb8b01a167c4b87dfe60d4408d757
farabek <farhodmuhamadiev4@gmail.com>
2024-12-04 11:06:36 +0500

    made HW11

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,3 +1,54 @@
+// import React from 'react'
+// import {useDispatch, useSelector} from 'react-redux'
+// import {AppStoreType} from './bll/store'
+// import {loadingAC} from './bll/loadingReducer'
+// import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
+// import s2 from '../../s1-main/App.module.css'
+// import {Loader} from './Loader'
+
+// /*
+// * 1 - в файле loadingReducer.ts дописать типы и логику
+// * 2 - получить isLoading из редакса
+// * 3 - дописать функцию setLoading
+// * 4 - сделать стили в соответствии с дизайном
+// * */
+
+// const HW10 = () => {
+//     // useSelector, useDispatch // пишет студент
+//     const isLoading = false
+
+//     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+//         // dispatch
+
+//         // setTimeout
+//     }
+
+//     return (
+//         <div id={'hw10'}>
+//             <div className={s2.hwTitle}>Homework #10</div>
+
+//             <div className={s2.hw}>
+//                 {isLoading ? (
+//                     <div id={'hw10-loading'}>
+//                         <Loader/>
+//                     </div>
+//                 ) : (
+//                     <SuperButton
+//                         id={'hw10-button-start-loading'}
+//                         onClick={setLoading}
+//                     >
+//                         Set loading...
+//                     </SuperButton>
+//                 )}
+//             </div>
+//         </div>
+//     )
+// }
+
+// export default HW10
+
+//////////////////////////////////////////
+
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { AppStoreType } from './bll/store';
@@ -20,6 +71,26 @@ const HW10 = () => {
   const isLoading = useSelector<AppStoreType, boolean>((state) => state.loading.isLoading);
 
   const dispatch = useDispatch();
+
+  //   const setLoading = () => {
+  //     // пишет студент // показать крутилку на 1,5 секунд
+  //     // dispatch
+  //     // setTimeout
+  //   };
+
+  //   const setLoading = () => {
+  //     dispatch(loadingAC(true));
+
+  //     setTimeout(() => dispatch(loadingAC(false)), 1500);
+  //   };
+
+  //   //
+  //   const setLoading = () => {
+  //     dispatch(loadingAC(true)); // Set loading to true
+  //     setTimeout(() => {
+  //       dispatch(loadingAC(false)); // Set loading to false after 1.5 seconds
+  //     }, 1500);
+  //   };
 
   const setLoading = () => {
     console.log('Setting loading to true');
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,20 @@
 import s from './Loader.module.css';
 
 export const Loader = () => <div className={s.loader} />;
+
+/////////////////////////////////////////////
+
+// export const Loader = () => {
+//   console.log('Loader is rendering');
+//   return <div className="loader">Loading...</div>;
+// };
+
+////////////////////////////////
+
+// import React from 'react';
+// import s from './Loader.module.css';
+
+// export const Loader = React.memo(() => {
+//   console.log('Loader is rendering');
+//   return <div className={s.loader} />;
+// });
