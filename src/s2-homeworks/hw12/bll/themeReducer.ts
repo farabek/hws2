10b8b845f4e6f7c04c52fc832ea01789f7a9411c
farabek <farhodmuhamadiev4@gmail.com>
2024-12-05 12:24:53 +0500

    changed HW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,46 +1,25 @@
-// const initState = {
-//   themeId: 1,
-// };
-
-// export const themeReducer = (state = initState, action: any): any => {
-//   // fix any
-//   switch (action.type) {
-//     // дописать
-
-//     default:
-//       return state;
-//   }
-// };
-
-// export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }); // fix any
-
-// //
-
-//////////////////////////////////////////////
-
 const initState = {
   themeId: 1,
 };
 
-type InitStateType = typeof initState;
-
-export const themeReducer = (
-  // state = initState, action: any): any => {
-  state: InitStateType = initState,
-  action: ThemeActions,
-): InitStateType => {
+export const themeReducer = (state = initState, action: ThemeActionsType): typeof initState => {
   // fix any
   switch (action.type) {
     // дописать
+
     case 'SET_THEME_ID':
       return { ...state, themeId: action.id };
+
     default:
       return state;
   }
 };
 
-type ThemeActions = ReturnType<typeof changeThemeId>;
+export const changeThemeId = (id: number): ChangeThemeAT => ({
+  type: 'SET_THEME_ID',
+  id,
+}); // fix any
 
-// export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }); // fix any
-export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const);
-//
+type ChangeThemeAT = { type: 'SET_THEME_ID'; id: number };
+
+type ThemeActionsType = ChangeThemeAT;
